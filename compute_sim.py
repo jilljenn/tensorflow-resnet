5117fe10d2cb36f89e920d88f4cb1a5ca99e9780
Jill-Jênn Vie <vie@jill-jenn.net>
2018-06-28 14:00:13 -0700

    Handle closest neighbors

--- compute_sim.py
+++ compute_sim.py
@@ -1,11 +1,28 @@
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import json
+import os.path
 
 
-with open('hashes.json') as f:
-    hashes = json.load(f)
+# with open('hashes.json') as f:
+#     hashes = json.load(f)
 
-filenames, X = zip(*hashes.items())
-print(filenames)
-print(cosine_similarity(X))
+# # filenames, X = zip(*hashes.items())  # All
+# LIMIT = ['data/castle.jpg', 'data/resize.jpg', 'data/sample.jpg', 'data/224.jpg', 'data/result.jpg', 'data/sampled.jpg']
+# filenames = LIMIT
+# X = [hashes[filename] for filename in filenames]
+# print(filenames)
+# print(cosine_similarity(X))
+#print(hashes['data/224.jpg'])
+
+filenames = open('filenames.txt').read().splitlines()
+X = np.load('hashes.npy')
+sim = cosine_similarity(X)
+DEMO = '/Users/jin/Desktop/New Year/resized/IMG_20180104_191930.jpg'
+index = filenames.index(DEMO)
+neighbor_ids = sim[index, :].argsort()[::-1][:10]
+with open('results.html', 'w') as f:
+    f.write('<h1>Plus proches voisins de {}</h1>'.format(DEMO))
+    f.write('<img src="{}" style="border: 2px solid red" /><br />'.format(DEMO))
+    for index in neighbor_ids:
+        f.write('<img src="{}" />'.format(filenames[index]))
--- read.py
+++ read.py
@@ -5,6 +5,13 @@ import skimage.transform
 import numpy as np
 import glob
 import json
+import os.path
+from datetime import datetime
+
+
+TRAIN_FOLDER = '/Users/jin/Desktop/New Year/resized'
+#TRAIN_FOLDER = 'data'
+BATCH_SIZE = 40
 
 
 # returns image of shape [224, 224, 3]
@@ -19,8 +26,8 @@ def load_image(path, size=224):
     return resized_img
 
 def restore_session(session):
-    saver = tf.train.import_meta_graph('ResNet-L50.meta')
-    saver.restore(session, 'ResNet-L50.ckpt')
+    saver = tf.train.import_meta_graph('ResNet-L152.meta')
+    saver.restore(session, 'ResNet-L152.ckpt')
 
 def print_prob(prob):
     #print prob
@@ -28,11 +35,16 @@ def print_prob(prob):
 
     # Get top1 label
     top1 = synset[pred[0]]
-    print("Top1: ", top1)
+    # print("Top1: ", top1)
     # Get top5 label
     top5 = [synset[pred[i]] for i in range(5)]
-    print("Top5: ", top5)
-    return top1
+    # print("Top5: ", top5)
+    return top5
+
+def get_batches(l):
+    n = len(l)
+    for i in range(0, n, BATCH_SIZE):
+        yield l[i:i + BATCH_SIZE]
 
 def main():
     with tf.Session() as sess:
@@ -53,28 +65,42 @@ def main():
 
         print("graph restored")
 
-        filenames = glob.glob('data/*.jpg')
+        filenames = glob.glob(os.path.join(TRAIN_FOLDER, '*.jpg'))
 
-        resized_images = []
-        for filename in filenames:
-            resized_images.append(load_image(filename))
+        topfives = []
+        hashes = []
+        for batch in get_batches(filenames):
+            start = datetime.now()
+            resized_images = []
+            for filename in batch:
+                resized_images.append(load_image(filename))
 
-        real_batch = np.stack(resized_images)
-        print('real batch', real_batch.shape)
+            real_batch = np.stack(resized_images)
+            print('real batch', real_batch.shape)
 
-        # batch = img.reshape((1, 224, 224, 3))
-        # print(batch.shape)
+            # batch = img.reshape((1, 224, 224, 3))
+            # print(batch.shape)
 
-        ranks, hashes = sess.run([prob_tensor, resnet_hash], feed_dict={images: real_batch})
-        all_hashes = {}
-        for filename, prob, img_hash in zip(filenames, ranks, hashes):
-            print(filename)
-            print_prob(prob)
-            all_hashes[filename] = img_hash.tolist()
-        
-        np.save('hashes.npy', hashes)
-        with open('hashes.json', 'w') as f:
-            f.write(json.dumps(all_hashes, indent=4))
+            ranks, batch_hashes = sess.run([prob_tensor, resnet_hash], feed_dict={images: real_batch})
+            hashes.append(batch_hashes)
+            # all_hashes = {}
+            
+            for filename, prob, img_hash in zip(batch, ranks, batch_hashes):
+                # print(filename)
+                topfives.append(str(print_prob(prob)))
+                # all_hashes[filename] = img_hash.tolist()
+            print('batch', datetime.now() - start)
+        all_hashes = np.vstack(hashes)
+        print('all hashes', all_hashes.shape)
+
+        with open('filenames.txt', 'w') as f:
+            f.write('\n'.join(filenames))
+        with open('topfives.txt', 'w') as f:
+            f.write('\n'.join(topfives))
+
+        np.save('hashes.npy', all_hashes.astype(np.float64))
+        # with open('hashes.json', 'w') as f:  # Will be deleted
+        #     f.write(json.dumps(all_hashes, indent=4))
 
 
 if __name__ == '__main__':
